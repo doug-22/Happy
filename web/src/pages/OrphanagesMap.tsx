fc2d17a1db0ae38a52c0fd91295fa133f7f72aff
Douglas Oliveira <douglas.aguiar410@gmail.com>
2020-10-18 00:11:14 -0300

    💄 chore: created orphanages page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanage-map.css';
 
 function OrphanagesMap() {
   return (
-    <h1>Hello World</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Barrreirinhas</strong>
+          <span>Maranhão</span>
+        </footer>
+      </aside>
+
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
   );
 }
 
