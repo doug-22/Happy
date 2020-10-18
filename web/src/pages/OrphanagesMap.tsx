dcfdd65a28890c3c6b097422693c54ffc0079f7b
Douglas Oliveira <douglas.aguiar410@gmail.com>
2020-10-18 00:35:00 -0300

    💄 chore: added map in orphanages page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
 
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
@@ -24,6 +26,15 @@ function OrphanagesMap() {
         </footer>
       </aside>
 
+      <Map
+        center={[-2.7507475,-42.8276535]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer 
+          url={"https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"}
+        />
+      </Map>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
