894e5ec3aca8ce4f84ce409d66c931ce8c0fc34e
Douglas Oliveira <douglas.aguiar410@gmail.com>
2020-10-17 23:47:50 -0300

    🚚 chore: created landing page route

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OrphanagesMap() {
+  return (
+    <div className="OrphanagesMap">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default OrphanagesMap;
